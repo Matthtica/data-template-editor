7fb04213ffae58b4ebcad42ab64c50045d9c4f83
Matthtica <zxnightstalker199733@gmail.com>
2024-03-21 10:48:00 +0000

    Button Has been replaced with LinkButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
+import LinkButton from "@/components/custom/link-button"
 import ModeToggle from "@/components/custom/mode-toggle"
-import NovelEditor from "@/components/editor"
-import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return <div className="mx-auto max-w-[40rem] flex flex-col">
     <div className="p-2 flex justify-between">
       <ModeToggle variant="ghost" />
-      <Button>New Template</Button>
+      <LinkButton href="/editor">New Template</LinkButton>
     </div>
     <h1 className="text-2xl font-bold m-3">List of template</h1>
     <div className="border border-input rounded-md shadow-md min-h-40">
