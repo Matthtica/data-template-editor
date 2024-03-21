068f1d25f794eab8a5b5cab718300846889521ea
Matthtica <zxnightstalker199733@gmail.com>
2024-03-21 10:51:15 +0000

    Tiptap added

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -14,6 +14,7 @@ import { handleCommandNavigation } from "novel/extensions";
 import { defaultExtensions } from "./extensions";
 import React, { useState } from "react";
 
+
 interface NovelEditorProps extends React.HTMLAttributes<HTMLDivElement> { }
 export default function NovelEditor({ className }: NovelEditorProps) {
   const [content, setContent] = useState<JSONContent | undefined>();
