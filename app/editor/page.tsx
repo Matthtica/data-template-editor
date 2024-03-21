0ec49c2b756358cf1e13d7e975b4494a8ca39cf8
Matthtica <zxnightstalker199733@gmail.com>
2024-03-21 07:42:20 +0000

    Editor page added

---
+++ app/editor/page.tsx
@@ -0,0 +1,7 @@
+import NovelEditor from "@/components/editor";
+
+export default function EditorPage() {
+  return <div>
+    <NovelEditor className="p-2 m-3 border border-input min-h-80 rounded shadow-lg" />
+  </div>
+}
