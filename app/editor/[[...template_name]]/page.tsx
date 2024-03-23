d6b2e9318fb364779b10e07abde9e3836d7d9650
Matthtica <zxnightstalker199733@gmail.com>
2024-03-23 23:43:05 +0630

    Editor page can now accept params to edit template

--- app/editor/[[...template_name]]/page.tsx
+++ app/editor/[[...template_name]]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import NovelEditor from "@/components/editor";
 import React from "react";
-import { JSONContent } from "novel";
+import { useRouter, useSearchParams } from "next/navigation";
 import {
   Dialog,
   DialogContent,
@@ -16,8 +16,17 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useTemplateSaver } from "./hooks";
+import { DBNAME } from "@/lib/constants";
+import { TemplateMap } from "@/lib/types";
+import Tiptap from "@/components/tiptap/tiptap";
 
-export default function EditorPage() {
+interface EditorPageRouteParams {
+  params: {
+    template_name?: string[]
+  }
+}
+
+export default function EditorPage({ params }: EditorPageRouteParams) {
   const {
     content, setContent,
     isModelOpen, setIsModelOpen,
@@ -25,8 +34,22 @@ export default function EditorPage() {
     name, setName
   } = useTemplateSaver();
 
+  React.useEffect(() => {
+    if (!params.template_name) return;
+    const template_name = params.template_name[0];
+    const stored_object = window.localStorage.getItem(DBNAME);
+    if (!stored_object) return;
+    const database = JSON.parse(stored_object);
+
+    const template: TemplateMap | undefined = database[template_name];
+    if (template) {
+      setContent(template);
+    }
+    console.log(template);
+  }, [])
+
   return <div className="flex flex-col h-screen p-3 gap-3 w-full items-center max-w-[55em] mx-auto">
-    <NovelEditor content={content} setContent={setContent} className="flex-1 bg-secondary rounded-md border border-input w-full p-2 px-3"/>
+    <Tiptap />
 
     <div className="flex justify-end w-full">
       <Dialog open={isModelOpen} onOpenChange={setIsModelOpen}>
