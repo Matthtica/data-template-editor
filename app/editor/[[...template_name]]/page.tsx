80c6d28e4700273a015a635d9592a7c9af25da28
Matthtica <zxnightstalker199733@gmail.com>
2024-03-25 23:01:45 +0630

    Used template saver UI component

--- app/editor/[[...template_name]]/page.tsx
+++ app/editor/[[...template_name]]/page.tsx
@@ -1,24 +1,11 @@
 'use client'
-import NovelEditor from "@/components/editor";
 import React from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  DialogHeader,
-  DialogTrigger,
-  DialogFooter,
-  DialogClose
-} from "@/components/ui/dialog";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { useTemplateSaver } from "./hooks";
-import { DBNAME } from "@/lib/constants";
-import { TemplateMap } from "@/lib/types";
 import Tiptap from "@/components/tiptap/tiptap";
+import ModeToggle from "@/components/custom/mode-toggle";
+import { defaultExtensions } from "@/components/tiptap/extensions";
+import { useEditor } from "@tiptap/react";
+import TemplateSaver from "./components/template-saver";
+import { getCurrentContent } from "@/lib/utils";
 
 interface EditorPageRouteParams {
   params: {
@@ -27,58 +14,19 @@ interface EditorPageRouteParams {
 }
 
 export default function EditorPage({ params }: EditorPageRouteParams) {
-  const {
-    content, setContent,
-    isModelOpen, setIsModelOpen,
-    saveToLocalStorage,
-    name, setName
-  } = useTemplateSaver();
+  const content = getCurrentContent({ params });
 
-  React.useEffect(() => {
-    if (!params.template_name) return;
-    const template_name = params.template_name[0];
-    const stored_object = window.localStorage.getItem(DBNAME);
-    if (!stored_object) return;
-    const database = JSON.parse(stored_object);
+  const editor = useEditor({
+    extensions: [...defaultExtensions],
+    content: content ?? '<p>Hello World!</p>',
+  });
 
-    const template: TemplateMap | undefined = database[template_name];
-    if (template) {
-      setContent(template);
-    }
-    console.log(template);
-  }, [])
-
+  if (!editor) return;
   return <div className="flex flex-col h-screen p-3 gap-3 w-full items-center max-w-[55em] mx-auto">
-    <Tiptap />
-
-    <div className="flex justify-end w-full">
-      <Dialog open={isModelOpen} onOpenChange={setIsModelOpen}>
-        <DialogTrigger asChild>
-          <Button>
-            Save Template
-          </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>
-              Give a name for the template
-            </DialogTitle>
-          </DialogHeader>
-          <DialogDescription>
-            If the name already exists, it will be overwritten
-          </DialogDescription>
-          <div>
-            <Label htmlFor="name">Name</Label>
-            <Input id="name" type="text" value={name} onChange={(event) => setName(event.target.value)}/>
-          </div>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button onClick={saveToLocalStorage}>Confirm</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+    <div className="flex justify-between w-full">
+      <ModeToggle variant="ghost"/>
+      <TemplateSaver editor={editor}/>
     </div>
+    <Tiptap editor={editor} className="w-full rounded-md border border-input min-h-[55rem]"/>
   </div>
 }
