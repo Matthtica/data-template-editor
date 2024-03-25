dd1a11b9652d39331930c0272a455308a7947b4f
Matthtica <zxnightstalker199733@gmail.com>
2024-03-25 23:06:46 +0630

    Tiptap editor wrapper simplified

--- components/tiptap/tiptap.tsx
+++ components/tiptap/tiptap.tsx
@@ -1,22 +1,21 @@
 'use client'
 import {
   EditorContent,
-  useEditor,
   Editor,
+  EditorProvider
 } from '@tiptap/react'
 import React from 'react';
 import { NodeSelector } from './node-selector';
 import { LinkSelector } from './link-selector';
 import { TextButtons } from './text-buttons';
 import { ColorSelector } from './color-selector';
-import { defaultExtensions } from "./extensions";
 
-interface TiptapProps extends React.HTMLAttributes<HTMLDivElement> { }
-export default function Tiptap({ className }: TiptapProps) {
-  const editor = useEditor({
-    extensions: [...defaultExtensions],
-    content: '<p>Hello World!</p>',
-  })
+interface TiptapProps extends React.HTMLAttributes<HTMLDivElement> {
+  editor: Editor | null
+}
+
+// TODO: Stop passing editor as props down to the grand child by using editor provider
+export default function Tiptap({ editor, className }: TiptapProps) {
   return <div className={className}>
     <EditorMenu editor={editor}/>
     <EditorContent editor={editor}/>
@@ -31,9 +30,11 @@ function EditorMenu({ editor }: EditorMenuProps) {
   const [openNode, setOpenNode] = React.useState(false);
   const [openLink, setOpenLink] = React.useState(false);
   const [openColor, setOpenColor] = React.useState(false);
-  return <div className="flex w-full bg-cyan-500">
+
+  return <div className="flex w-full bg-secondary">
     <NodeSelector editor={editor} open={openNode} onOpenChange={setOpenNode}/>
     <LinkSelector editor={editor} open={openLink} onOpenChange={setOpenLink}/>
     <TextButtons editor={editor}/>
+    <ColorSelector editor={editor} open={openColor} onOpenChange={setOpenColor}/>
   </div>
 }
