55a84896307fd84c7ff05d8fed39fdf7c207c49b
Matthtica <zxnightstalker199733@gmail.com>
2024-03-25 23:03:50 +0630

    Necessary type for DataMap and EditorPageRouteParams

--- lib/types.ts
+++ lib/types.ts
@@ -1,5 +1,13 @@
-import { JSONContent } from "novel";
-
 export interface TemplateMap {
-  [key: string]: JSONContent
+  [key: string]: string
+}
+
+export interface DataMap {
+  [key: string]: string
+}
+
+export interface EditorPageRouteParams {
+  params: {
+    template_name?: string[]
+  }
 }
