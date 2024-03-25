35d40ea63989ec67c16c8de0d5cbe9bc4bb9b977
Matthtica <zxnightstalker199733@gmail.com>
2024-03-25 23:00:58 +0630

    simplified hook for the editor page

--- app/editor/[[...template_name]]/hooks.ts
+++ app/editor/[[...template_name]]/hooks.ts
@@ -1,41 +1,29 @@
 import { toast } from "@/components/ui/use-toast";
-import { DBNAME } from "@/lib/constants";
-import { JSONContent } from "novel";
 import React from "react";
-import { TemplateMap } from "@/lib/types";
+import { EditorPageRouteParams } from "@/lib/types";
+import { getLocalStorageDB, setLocalStorageDB } from "@/lib/utils";
 
-
-export function useTemplateSaver() {
-  const [content, setContent] = React.useState<JSONContent | undefined>();
+// helper function with a dialog for the name of the template to be saved
+export function useTemplateSaver(setIsOpen: (value: boolean) => void) {
   const [name, setName] = React.useState("");
-  const [isOpen, setIsOpen] = React.useState(false);
 
-  const handleOnSave = () => {
-    if (name === "") {
-      return
-    }
-    let database: TemplateMap = {};
-    const existing_db = window.localStorage.getItem(DBNAME);
-    if (existing_db) {
-      database = JSON.parse(existing_db);
-    }
+  const saveContent = (content: string) => {
+    if (name === "") return;
+    let database = getLocalStorageDB();
     if (content) {
       database[name] = content;
     }
-    window.localStorage.setItem(DBNAME, JSON.stringify(database));
+    setLocalStorageDB(database);
     setName("");
     setIsOpen(false);
     toast({
       title: "Saved Template",
       description: `The template ${name} has been save to the local storage`
-    })
+    });
   }
   return {
-    isModelOpen: isOpen,
-    setIsModelOpen: setIsOpen,
-    content,
-    setContent,
     name, setName,
-    saveToLocalStorage: handleOnSave
+    saveContent
   };
 }
+
