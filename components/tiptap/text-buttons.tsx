0db8b1e2902c178c4e262570d827dc889fca7369
Matthtica <zxnightstalker199733@gmail.com>
2024-03-26 14:09:53 +0630

    Editor is not null anymore

--- components/tiptap/text-buttons.tsx
+++ components/tiptap/text-buttons.tsx
@@ -9,38 +9,40 @@ interface TextButtonsProps {
 }
 
 export function TextButtons({ editor }: TextButtonsProps) {
+  if (!editor) return null;
   const items: SelectorItem[] = [
     {
       name: "bold",
-      isActive: (editor) => editor?.isActive("bold") ?? false,
-      command: (editor) => editor?.chain().focus().toggleBold().run(),
+      isActive: (editor) => editor.isActive("bold") ?? false,
+      command: (editor) => editor.chain().focus().toggleBold().run(),
       icon: BoldIcon,
     },
     {
       name: "italic",
-      isActive: (editor) => editor?.isActive("italic") ?? false,
-      command: (editor) => editor?.chain().focus().toggleItalic().run(),
+      isActive: (editor) => editor.isActive("italic") ?? false,
+      command: (editor) => editor.chain().focus().toggleItalic().run(),
       icon: ItalicIcon,
     },
     {
       name: "underline",
-      isActive: (editor) => editor?.isActive("underline") ?? false,
-      command: (editor) => editor?.chain().focus().toggleUnderline().run(),
+      isActive: (editor) => editor.isActive("underline") ?? false,
+      command: (editor) => editor.chain().focus().toggleUnderline().run(),
       icon: UnderlineIcon,
     },
     {
       name: "strike",
-      isActive: (editor) => editor?.isActive("strike") ?? false,
-      command: (editor) => editor?.chain().focus().toggleStrike().run(),
+      isActive: (editor) => editor.isActive("strike") ?? false,
+      command: (editor) => editor.chain().focus().toggleStrike().run(),
       icon: StrikethroughIcon,
     },
     {
       name: "code",
-      isActive: (editor) => editor?.isActive("code") ?? false,
-      command: (editor) => editor?.chain().focus().toggleCode().run(),
+      isActive: (editor) => editor.isActive("code") ?? false,
+      command: (editor) => editor.chain().focus().toggleCode().run(),
       icon: CodeIcon,
     },
   ];
+
   return <div className='flex'>
       {items.map((item, index) => (
         <Button key={index} size='icon' variant='ghost'
