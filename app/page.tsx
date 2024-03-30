b8e9bc726357119ed425fad95c9adcb4976a32be
Matthtica <zxnightstalker199733@gmail.com>
2024-03-30 14:59:52 +0630

    An attempt at fixing minifying code error

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,19 @@ import React from "react"
 
 export default function Home() {
   const { templates } = useTemplateStorageContext();
+  const [isMounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, [])
+  if (!isMounted) return null;
 
   return <div className="mx-auto max-w-[40rem] flex flex-col">
     <div className="p-2 flex justify-between">
       <ModeToggle variant="ghost" />
       <LinkButton href="/editor">New Template</LinkButton>
     </div>
-    <h1 className="text-2xl font-bold m-3">Here is list of template</h1>
+    <h1 className="text-2xl font-bold m-3">List of template</h1>
     <div className="rounded-md shadow-lg min-h-40 p-3 flex flex-col gap-3">
       {templates.map((template) => {
         return <TemplateItem key={template.id} template={template}/>
