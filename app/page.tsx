9853f63216216392dbb9e93b651c01611fb5eaa5
Matthtica <zxnightstalker199733@gmail.com>
2024-03-30 14:37:27 +0630

    Main page simplified with the useTemplateStorageContext hook

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,11 @@
 import LinkButton from "@/components/custom/link-button"
 import ModeToggle from "@/components/custom/mode-toggle"
 import TemplateItem from "@/components/custom/template-item";
-import { DBNAME } from "@/lib/constants";
-import { TemplateMap } from "@/lib/types";
+import { useTemplateStorageContext } from "@/context/TemplateStorageContext";
 import React from "react"
 
 export default function Home() {
-  const [names, setNames] = React.useState<string[]>();
-  const [db, setDb] = React.useState<TemplateMap>();
-
-  React.useEffect(() => {
-    const stored_object = window.localStorage.getItem(DBNAME);
-    if (stored_object) {
-      let database: TemplateMap = JSON.parse(stored_object);
-      setNames(Object.keys(database));
-      setDb(database);
-    }
-  }, [])
+  const { templates } = useTemplateStorageContext();
 
   return <div className="mx-auto max-w-[40rem] flex flex-col">
     <div className="p-2 flex justify-between">
@@ -25,9 +14,9 @@ export default function Home() {
       <LinkButton href="/editor">New Template</LinkButton>
     </div>
     <h1 className="text-2xl font-bold m-3">List of template</h1>
-    <div className="border border-input rounded-md shadow-md min-h-40 p-3 flex flex-col gap-3">
-      {names?.map((name) => {
-        return <TemplateItem key={name} filename={name}/>
+    <div className="rounded-md shadow-lg min-h-40 p-3 flex flex-col gap-3">
+      {templates.map((template) => {
+        return <TemplateItem template={template}/>
       })}
     </div>
   </div>
